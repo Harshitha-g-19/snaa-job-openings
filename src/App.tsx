9a3d03adeaa23865b5ebdd471e8ae74512f6a26e
HARSHITHA <DA@GBMTDC.LOCAL>
2026-04-20 11:38:31 +0530

    Minimal app test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index";
-import Careers from "./pages/Careers";
-import JobDetail from "./pages/JobDetail";
-import HRLogin from "./pages/HRLogin";
-import HRDashboard from "./pages/HRDashboard";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/careers/:id" element={<JobDetail />} />
-          <Route path="/admin-staff" element={<HRLogin />} />
-          <Route path="/admin-staff/dashboard" element={<HRDashboard />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <div style={{ padding: "40px", background: "white", color: "black" }}>
+      <h1>SNAA Careers</h1>
+      <p>App is working.</p>
+      <p>
+        <a href="/careers">Go to Careers</a>
+      </p>
+    </div>
+  );
+}
 
 export default App;
